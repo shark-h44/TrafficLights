cbe4f6c64548ee5729a15be80fc03b142a3a078f
shark-h44 <38717304+shark-h44@users.noreply.github.com>
2018-04-25 16:15:25 +0200

    Add files via upload

---
+++ control.ino
@@ -0,0 +1,77 @@
+/*
+ * Configure les broche de l'arduino
+ * Led en sortie
+ * Bouton en entrée (à venir)
+ */
+void setup_feux (void)
+{
+  pinMode(F1R, OUTPUT);
+  pinMode(F1J, OUTPUT);
+  pinMode(F1V, OUTPUT);
+
+  pinMode(F2R, OUTPUT);
+  pinMode(F2J, OUTPUT);
+  pinMode(F2V, OUTPUT);
+
+  pinMode(F3R, OUTPUT);
+  pinMode(F3J, OUTPUT);
+  pinMode(F3V, OUTPUT);
+
+  pinMode(F4R, OUTPUT);
+  pinMode(F4J, OUTPUT);
+  pinMode(F4V, OUTPUT); 
+}
+
+
+
+/*
+ * Allume le feu correspondant à l'identifiant passé
+ */
+void allume_feu (byte IdFeu) 
+{
+  digitalWrite(IdFeu, HIGH);
+}
+
+/*
+ * Allume le feu correspondant à l'identifiant passé
+
+ */
+void eteins_feu (byte IdFeu) 
+{
+  digitalWrite(IdFeu, LOW);
+}
+
+
+/*
+ * Allume le feu correspondant à l'identifiant passé
+ */
+void clignote_feu (byte IdFeu, int nb_cligno=4, int tempo=1000)
+{
+  for(int i=0; i<nb_cligno; i++){
+    digitalWrite(IdFeu, HIGH);
+    delay(tempo);
+    digitalWrite(IdFeu, LOW);
+    delay(tempo);
+  }  
+}
+
+void test_LED (int tempo=200)
+{
+  clignote_feu (F1R, 1, tempo);
+  clignote_feu (F1J, 1, tempo);
+  clignote_feu (F1V, 1, tempo);
+  
+  clignote_feu (F2R, 1, tempo);
+  clignote_feu (F2J, 1, tempo);
+  clignote_feu (F2V, 1, tempo);
+
+  clignote_feu (F3R, 1, tempo);
+  clignote_feu (F3J, 1, tempo);
+  clignote_feu (F3V, 1, tempo);
+  
+  clignote_feu (F4R, 1, tempo);
+  clignote_feu (F4J, 1, tempo);
+  clignote_feu (F4V, 1, tempo);
+}
+
+
---
+++ feux_tricolores2.ino
@@ -0,0 +1,48 @@
+byte F1R = 16;
+byte F1J = 15;
+byte F1V = 14;
+byte F2R = 4;
+byte F2J = 5;
+byte F2V = 6;
+byte F3R = 7;
+byte F3J = 8;
+byte F3V = 9;
+byte F4R = 10;
+byte F4J = 11;
+byte F4V = 12;
+
+/*
+ * Déclaration des prototypes des fonctions de contrôle
+ */
+void setup_feux (void);
+void allume_feu (byte IdFeu);
+void eteins_feu (byte IdFeu);
+void clignote_feu (byte IdFeu, int nb_cligno=4, int tempo=1000);
+void test_LED (int tempo=200);
+
+/*
+ * Initialisation du programme (effectué une seule fois)
+ */
+void setup() 
+{
+  setup_feux();
+}
+
+/*
+ * Boucle du programme principal (boule infinie)
+ */
+void loop() 
+{
+test_LED(1000);
+
+/*
+allume_feu(F1R);
+allume_feu(F3R);
+delay(5000)
+
+allume_feu(F1R);
+allume_feu(F3R);
+delay(5000)
+*/
+}
+
